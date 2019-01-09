bd1a7d689a486a03a203885589d9e4033837f89e
Rifqi Sulistya <rifqi.sulistya@gmail.com>
2019-01-09 12:52:43 +0700

    sampai delete post

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Alert, AppRegistry, Button, Dimensions, FlatList, Platform, StyleSheet, Text, TextInput, View} from 'react-native';
-
+import {Alert, AppRegistry, Button, Dimensions, FlatList, Image, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
+import LinkPreview from 'react-native-link-preview';
+import axios from 'react-native-axios';
 
 export default class App extends Component<Props> {
   constructor(props) {
     super(props);
     this.state = {
       text: '',
-      post: []
+      posts: [],
+      title : "sample title"
     };
   }
 
@@ -23,16 +17,94 @@ export default class App extends Component<Props> {
     this.setState({text})
   }
 
-  handlePress = () => {
+  handleHapus = (key) => {
+    var posts = this.state.posts
+    var newPost = []
+    for (i=0; i<posts.length; i++){
+      if (key != i){
+        newPost.push(posts[i])
+      }
+    }
     this.setState(
       {
-        post:[{text:this.state.text},...this.state.post],
-        text:''
-      }
+        posts : newPost
+      }    
     )
+    console.log(key)
+    axios.delete('http://192.168.1.17:8081/mysql/'+key)
+    .then(function (response) {
+      alert(response.data)
+      console.log(response);
+
+    })
+    .catch(function (error) {
+      alert(error.message)
+      console.log(error);
+    }); 
+  }
+  
+  handlePress = () => {
+    var regex = /(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])?/;
+    if (this.state.text == ""){
+      alert('require input')
+    }
+    else if (this.state.text.match(regex)) {
+      LinkPreview.getPreview(this.state.text)
+      .then(data => {
+        this.setState(
+          {
+            posts:
+            [
+              {
+                //text:this.state.text,
+                title:data.title, 
+                img:data.images[0],
+                dsc:data.description,
+                post:this.state.text
+              },
+              ...this.state.posts
+            ],
+            text:''
+          }
+        );
+        axios.post('http://192.168.1.17:8081/mysql', 
+          {test:'aa'}
+        )
+        .then(function (response) {
+          alert(response.data)
+          console.log(response);
+
+        })
+        .catch(function (error) {
+          alert(error.message)
+          console.log(error);
+        });
+      });
+    }
+    else {
+      this.setState(
+        {
+          posts:[{text:this.state.text},...this.state.posts],
+          text:''
+        }
+      )
+    }  
+  }
+
+   componentDidMount () {
+    axios.get("http://192.168.1.17:8081/mysql")
+      .then (response => {
+        this.setState({
+          posts: response.data
+        })
+      })
+      .catch(error => {
+        console.log('err', error)
+      }); 
   }
 
   renderItem = ({ item, index }) => {
+    console.log(item)
     if (item.empty === true) {
       return <View style={[styles.item, styles.itemInvisible]} />;
     }
@@ -41,24 +113,18 @@ export default class App extends Component<Props> {
         style={styles.item}
       >
         <Text style={styles.itemText}>{item.text}</Text>
+        <Text style={styles.itemText}>{item.title}</Text>
+        <Image source={{uri:item.img}} style={{width:100, height:100}}/>
+        <Text>{item.dsc}</Text>
+        <Button
+          onPress={() => this.handleHapus(index)}
+          title="x"
+        />
       </View>
     );
   };
 
-  // componentDidMount () {
-  //   axios.get("http://localhost:8080/blogmobile")
-  //     .then (response => {
-  //       this.setState({
-  //         post: response.data
-  //       })
-  //       console.log(">>>response",response);
-  //     })
-  //     .catch(error => {
-  //       console.log('err', error)
-  //     }); 
-  // }
   render() {
-    console.log(this.state.post)
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to Blog Mobile App!</Text>
@@ -73,11 +139,10 @@ export default class App extends Component<Props> {
           title="Post it!"
         />
         <FlatList
-          data={this.state.post}
-          //renderItem={({item}) => <Text>{item.text}</Text>}
+          data={this.state.posts}
           renderItem={this.renderItem}
-        />
-            
+          keyExtractor = {(item,index) => item.id.toString()}
+        />            
       </View>
     );
   }
@@ -101,8 +166,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     flex: 1,
     margin: 1,
-    height: 40,
-    width: 350, // approximate a square
+    height:200,
+    width: 350, 
   },
   itemInvisible: {
     backgroundColor: 'transparent',
@@ -111,3 +176,12 @@ const styles = StyleSheet.create({
     color: '#fff',
   },
 });
+
+
+//pake if buat regex buat detect url
+//nulis tentang mongodb
+//ganti db nya jadi mongodb:
+//pake mysql bisa create & read. RN <-> Express <-> Mysql
+//key extractor buat delete. pake iterasi.
+
+//bulan ini crud
